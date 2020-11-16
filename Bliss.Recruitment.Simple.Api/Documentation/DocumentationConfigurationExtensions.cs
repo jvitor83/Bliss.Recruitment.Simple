26bb0b059c4826baeca70987616b217826017554
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2020-11-15 21:34:13 -0400

    feat(healthcheck): added healthcheck for database

---
+++ Bliss.Recruitment.Simple.Api/Documentation/DocumentationConfigurationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bliss.Recruitment.Simple.Api.Documentation
+{
+    public static class DocumentationConfigurationExtensions
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bliss.Recruitment.Simple.Api", Version = "v1" });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bliss.Recruitment.Simple.Api v1"));
+
+            return app;
+        }
+
+    }
+}
---
+++ Bliss.Recruitment.Simple.Api/HealthCheck/HealthCheckConfigurationExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Bliss.Recruitment.Simple.Api.HealthCheck
+{
+    public static class HealthCheckConfigurationExtensions
+    {
+        public static IServiceCollection AddHealthCheckConfiguration(this IServiceCollection services, string databaseConnectionString)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(connectionString: databaseConnectionString,
+                name: "sqlserver",
+                failureStatus: HealthStatus.Unhealthy,
+                timeout: TimeSpan.FromSeconds(5));
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseHealthCheckConfiguration(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/health",
+               new HealthCheckOptions
+               {
+                   ResponseWriter = async (context, report) =>
+                   {
+                       var result = System.Text.Json.JsonSerializer.Serialize(
+                           new
+                           {
+                               status = HelthCheckStatusToString(report.Status),
+                           });
+                       context.Response.ContentType = MediaTypeNames.Application.Json;
+                       await context.Response.WriteAsync(result);
+                   }
+               });
+
+
+            return app;
+        }
+
+        private static string HelthCheckStatusToString(HealthStatus status)
+        {
+            switch (status)
+            {
+                case HealthStatus.Unhealthy:
+                    return "Service Unavailable. Please try again later.";
+                    break;
+                case HealthStatus.Degraded:
+                    return "Service unstable, proceed with caution.";
+                    break;
+                case HealthStatus.Healthy:
+                    return "OK";
+                    break;
+                default:
+                    return "Unknown";
+                    break;
+            }
+        }
+
+
+    }
+}
